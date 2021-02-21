bddf7ec57dd54c7077f315a79d235fded296300a
MadeshG <74277922+MadeshG@users.noreply.github.com>
2021-02-21 14:12:12 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PRo-35-
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,37 @@
+var canvas,backgroungImage
+var database
+var balloon,balloonImage
+
+
+
+function preload(){
+  backgroundImage = loadImage('Images/Hot Air Ballon-01.png')
+  balloonImage = loadImage('Images/Hot Air Ballon-02.png')
+}
+
+function setup() {
+  createCanvas(500,500);
+ balloon = createSprite(250, 250, 50, 50);
+database = firebase.database()
+balloon.addImage(balloonImage)
+}
+
+
+function draw() {
+  background(255,255,255); 
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.x = balloon.x -10
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.x = balloon.x +10
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.y = balloon.y -10
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.y = balloon.y +10
+  }
+  drawSprites();
+
+}
